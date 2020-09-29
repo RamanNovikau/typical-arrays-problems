527d6387d335ff0b03e04795d4a1ea563bb94578
Роман Новиков <Demoi@rnovikov-pc.bsw.iron>
2020-09-29 08:20:37 +0300

    feat: add array min function

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    var min = 0;
+    if (!array) return min;
+    if (!array.length) return min;
+    min = array[0];
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] < min) min = array[i];
 }
+    return min;
+};
 
 exports.max = function max (array) {
   return 0;
